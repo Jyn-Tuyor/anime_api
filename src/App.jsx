64512ccabd93711707e48a510cb7182b335e8b94
jinnn888 <tuyor.jynchris@gmail.com>
2024-06-25 23:46:06 +0800

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Header from '/src/components/Header.jsx';
 import Content from '/src/components/Content.jsx';
 import '/src/index.css';
@@ -9,46 +9,72 @@ export default function App() {
   const [loadingState, setLoadingState] = useState(null)
   const [error, setError] = useState(null)
   const [search, setSearch] = useState('')
+  const [menu, setMenu] = useState(false)
+  const menuRef = useRef(null)
   const url = `https://api.jikan.moe/v4/anime?q=${search}&sfw`;
 
   function handleSubmit(e) {
-      e.preventDefault()   
-      setSearch('') 
-      setLoadingState(true)
-      fetch(url)
-      .then((res) =>  {
-        if (!res.ok) {
-          throw new Error('HTTP error status', res.status)
-        }
-        return res.json()
-      })
-      .then((data) => {
-        if (data && data.data) {
-          const imgUrl = data.data.map((img) => img.images.jpg.image_url)
-          setData(data)
-          setImage(imgUrl)
-          setLoadingState(false)
-        } else {
-          throw new Error('No data available')
-        }
-      })
-      .catch((err) =>  {
-        setError('404 Error: Failed fetching... :<')
+    e.preventDefault()   
+    setSearch('') 
+    setLoadingState(true)
+    fetch(url)
+    .then((res) =>  {
+      if (!res.ok) {
+        throw new Error('HTTP error status', res.status)
+      }
+      return res.json()
+    })
+    .then((data) => {
+      if (data && data.data) {
+        const imgUrl = data.data.map((img) => img.images.jpg.image_url)
+        setData(data)
+        setImage(imgUrl)
         setLoadingState(false)
-      });
+      } else {
+        throw new Error('No data available')
+      }
+    })
+    .catch((err) =>  {
+      setError('404 Error: Failed fetching... :<')
+      setLoadingState(false)
+    });
+  }
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setMenu(false)
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+  }, [menu])
+
+  const handleMenu = () => {
+    setMenu(!menu)
   }
   return (
     <>
-      <Header handleSubmit={handleSubmit} search={search} setSearch={setSearch}/>
-      {loadingState && <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center">
-          <div className="absolute bg-black opacity-50 w-full h-full"></div>
-          <div className="modal-container z-50 mx-auto rounded-lg overflow-hidden bg-white shadow-lg max-w-lg">
-            <div className="modal-content p-4">
-              <h2 className="text-lg font-bold mb-2">Fetching</h2>
-            </div>
-          </div>
-        </div>}
-      <Content image={image} data={data}/>
+    <Header 
+    handleSubmit={handleSubmit} 
+    search={search} 
+    setSearch={setSearch}  
+    handleMenu={handleMenu} 
+    menu={menu}
+    menuRef={menuRef}
+    />
+    {loadingState && <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center">
+    <div className="absolute bg-black opacity-50 w-full h-full"></div>
+    <div className="modal-container z-50 mx-auto rounded-lg overflow-hidden bg-white shadow-lg max-w-lg">
+    <div className="modal-content p-4">
+    <h2 className="text-lg font-bold mb-2">Fetching</h2>
+    </div>
+    </div>
+    </div>}
+    <Content image={image} data={data}/>
     </>
-  );
+    );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import '/src/index.css'
 
-function Header({handleSubmit, search, setSearch}) {
+function Header({handleSubmit, search, setSearch, handleMenu, menu, menuRef}) {
 	return (
 	<form onSubmit={handleSubmit}
 		className="container-header w-full text-white text-2xl font-bold bg-blue-700 px-4 py-2 mb-5 rounded">
     	<h1><a href='/Anime-Search-App/'>Search Anime</a></h1>
-    	<input
+    	<div className={`input-container ${menu ? 'active' : ''}`}>
+    		
+    		<input
     		autoFocus
+    		placeholder='Search'
     		className='rounded'
     		type='text'
     		value={search}
@@ -17,6 +20,9 @@ function Header({handleSubmit, search, setSearch}) {
     		paddingLeft: '12px'
     	}}
     	/>
+
+    	</div>
+    	
     	<button 
     	style={{
     		fontSize: '1rem',
@@ -27,6 +33,14 @@ function Header({handleSubmit, search, setSearch}) {
     		border: '2px solid steelblue'
     	}}
     	type='submit'>Submit</button>
+    	<div 
+    	onClick={handleMenu}
+    	ref={menuRef}
+    	className={`menu ${menu ? 'active' : ''}`}>
+    		<div className={`bar bar1 ${menu ? 'active' : ''}`}></div>
+    		<div className={`bar bar2 ${menu ? 'active' : ''}`}></div>
+    		<div className={`bar bar3 ${menu ? 'active' : ''}`}></div>
+    	</div>
 	</form>
 	)
 
